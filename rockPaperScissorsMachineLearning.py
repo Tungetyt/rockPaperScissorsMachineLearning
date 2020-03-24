28228b3ba66f94dac745feb51c1fc3f22cef608e
MK <m0kotlicki@gmail.com>
2020-03-24 14:24:28 +0100

    .

--- rockPaperScissorsMachineLearning.py
+++ rockPaperScissorsMachineLearning.py
@@ -4,32 +4,38 @@ import numpy as np
 
 
 def main():
-    rps = ['r', 'p', 's']
     payment = 0
     payment_history = [payment]
+
+    rps = ['r', 'p', 's']
+    rps_history = np.array([[1, 1, 1],
+                            [1, 1, 1],
+                            [1, 1, 1]]
+                           , np.int32)
+
     is_first_round = True
     last_user_input = 'no_input'
     end_game_key = 'e'
     info_message = f'Enter: \n{rps[0]} for rock, \n{rps[1]} for paper, \n{rps[2]} for scissors. \n{end_game_key} to end the game.'
     start_end_message = '-----------------------------'
-    rps_matrix = np.array([[1, 1, 1],
-                           [1, 1, 1],
-                           [1, 1, 1]]
-                          , np.int32)
+
     print(start_end_message)
     print(info_message)
 
     while True:
         print(start_end_message)
+
         user_input = input().casefold()
 
         if user_input == end_game_key:
             print('game has ended')
             print(start_end_message)
+
             plt.plot(payment_history)
             plt.xlabel('rounds')
             plt.ylabel('payment')
             plt.show()
+
             break
 
         if user_input not in rps:
@@ -39,13 +45,13 @@ def main():
         if is_first_round:
             computer_choice = np.random.choice(rps)
             is_first_round = False
-            print(rps_matrix)
+            print(rps_history)
         elif last_user_input == rps[0]:
-            computer_choice = comp_choose_and_learn(rps, rps_matrix, user_input, 0)
+            computer_choice = comp_choose_and_learn(rps, rps_history, user_input, 0)
         elif last_user_input == rps[1]:
-            computer_choice = comp_choose_and_learn(rps, rps_matrix, user_input, 1)
+            computer_choice = comp_choose_and_learn(rps, rps_history, user_input, 1)
         else:
-            computer_choice = comp_choose_and_learn(rps, rps_matrix, user_input, 2)
+            computer_choice = comp_choose_and_learn(rps, rps_history, user_input, 2)
 
         print(computer_choice)
 
