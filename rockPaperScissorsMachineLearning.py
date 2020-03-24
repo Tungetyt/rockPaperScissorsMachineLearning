bd26c466e6560b9d5b7c749e668961faded9f3e4
MK <m0kotlicki@gmail.com>
2020-03-24 13:17:06 +0100

    program works

--- rockPaperScissorsMachineLearning.py
+++ rockPaperScissorsMachineLearning.py
@@ -7,27 +7,36 @@ def main():
     choices = ['r', 'p', 's']
     payment = 0
     is_first_round = True
-    last_user_input = '0'
-    computer_choice = ''
+    last_user_input = 'no_input'
+    end_game_key = 'e'
+    info_message = f'Enter: \n{choices[0]} for rock, \n{choices[1]} for paper, \n{choices[2]} for scissors. \n{end_game_key} to end the game.'
+    start_end_message = '-----------------------------'
     rps_matrix = np.array([[1, 1, 1],
                            [1, 1, 1],
                            [1, 1, 1]]
                           , np.int32)
+
+    print(start_end_message)
+    print(info_message)
+
     while True:
         # wybor usera
+        print(start_end_message)
         user_input = input().casefold()
 
-        # wybor compa
-        if user_input == 'end':
-            print('end')
+        if user_input == end_game_key:
+            print('game has ended')
+            print(start_end_message)
             break
 
         if user_input not in choices:
+            print(info_message)
             continue
 
         if is_first_round:
-            print(np.random.choice(choices))
+            computer_choice = np.random.choice(choices)
             is_first_round = False
+            print(rps_matrix)
         elif last_user_input == choices[0]:
             computer_choice = comp_choose_and_learn(choices, rps_matrix, user_input, 0)
         elif last_user_input == choices[1]:
@@ -35,68 +44,61 @@ def main():
         else:
             computer_choice = comp_choose_and_learn(choices, rps_matrix, user_input, 2)
 
-        # aktualizacja macierzy
+        print(computer_choice)
 
         # sprawdzenie kto wygral
         if user_input == computer_choice:
             print('tie! payment: {}'.format(payment))
         elif user_input == choices[0]:
             if computer_choice == choices[1]:
-                payment = evaluate_winner(False, payment)
+                payment = evaluate_winner(False, payment, start_end_message)
             elif computer_choice == choices[2]:
-                payment = evaluate_winner(True, payment)
+                payment = evaluate_winner(True, payment, start_end_message)
         elif user_input == choices[1]:
             if computer_choice == choices[2]:
-                payment = evaluate_winner(False, payment)
+                payment = evaluate_winner(False, payment, start_end_message)
             elif computer_choice == choices[0]:
-                payment = evaluate_winner(True, payment)
+                payment = evaluate_winner(True, payment, start_end_message)
         elif user_input == choices[2]:
             if computer_choice == choices[0]:
-                payment = evaluate_winner(False, payment)
+                payment = evaluate_winner(False, payment, start_end_message)
             elif computer_choice == choices[1]:
-                payment = evaluate_winner(True, payment)
+                payment = evaluate_winner(True, payment, start_end_message)
 
         last_user_input = user_input
-        print(user_input)
 
-def evaluate_winner(is_user_won, payment):
+
+def evaluate_winner(is_user_won, payment, start_end_round_message):
     if is_user_won:
         payment -= 1
         print('user wins! payment: {}'.format(payment))
-        return payment
     else:
         payment += 1
         print('cmp wins! payment: {}'.format(payment))
-        return payment
 
-# def checkWinner(userInput, computerChoice, choices):
+    return payment
+
+
 def comp_choose_and_learn(choices, rps_matrix, user_input, row_number):
-    rps_matrix[row_number]
-    lowest_value_index = 0
-    highest_value_index = 2
-    lowest_value = rps_matrix[row_number][0]
-    highest_value = 0
+    computer_guess = np.random.choice(choices, p=(rps_matrix[row_number] / sum(rps_matrix[row_number])))
 
-    for x in range(3):
-        curr_num = rps_matrix[row_number][x]
-        if curr_num >= highest_value:
-            highest_value = curr_num
-            highest_value_index = x
-        if curr_num < lowest_value:
-            lowest_value = curr_num
-            lowest_value_index = x
+    if computer_guess == choices[0]:
+        computer_choice = choices[1]
+    elif computer_guess == choices[1]:
+        computer_choice = choices[2]
+    else:
+        computer_choice = choices[0]
 
-    reversed_values_matrix = []
-
-    computer_choice = np.random.choice(choices, p= (rps_matrix[row_number] / sum(rps_matrix[row_number])))
-    print('cmp: {}'.format(computer_choice))
     if user_input == choices[0]:
         rps_matrix[row_number][0] += 1
     elif user_input == choices[1]:
         rps_matrix[row_number][1] += 1
     elif user_input == choices[2]:
         rps_matrix[row_number][2] += 1
+
     print(rps_matrix)
+
     return computer_choice
+
 
 main()
